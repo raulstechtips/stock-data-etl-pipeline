bf8a230b385990bd63a363095de0f454ae1130d5
chakraborty29 <chakrabortyraul@hotmail.com>
2025-12-28 07:44:22 +0000

    fixed string

--- services/api/tests/__init__.py
+++ services/api/tests/__init__.py
@@ -60,5 +60,5 @@ __all__ = [
     'QueueAllStocksForFetchAPITest',
     'BulkQueueRunFilterAPITest',
     'BulkQueueRunListAPITest',
-    BulkQueueRunListFilterAPITest,
+    'BulkQueueRunListFilterAPITest',
 ]
