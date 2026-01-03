0bcef38a66367fcca75f0c5ab7caa6ad587fdeff
chakraborty29 <chakrabortyraul@hotmail.com>
2026-01-03 08:09:09 +0000

    removed redundant logging import

--- services/config/settings.py
+++ services/config/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
-import logging
 from urllib.parse import quote
 from pathlib import Path
 from django.core.exceptions import ImproperlyConfigured
